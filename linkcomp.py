8843e48fbc301ed524c61acee6ef6e55eff4aec9
MattB-242 <58733062+MattB-242@users.noreply.github.com>
2020-09-23 12:22:09 +0100

    Update linkcomp.py

--- linkcomp.py
+++ linkcomp.py
@@ -15,6 +15,7 @@ import numpy as np
 import gemmi
 from gemmi import cif
 import sympy as sp
+import pandas as pd
 import csv
 
 #---------------------------------------------------------------------
@@ -22,16 +23,20 @@ import csv
 #---------------------------------------------------------------------
 
 # Check parameters
-if (len(sys.argv) != 3):
-    print("Must have 2 parameters: input dir and an output dir!")
+if (len(sys.argv) != 5):
+    print("Must have 4 parameters: input dir, output dir, and a start and end index in the file!")
     sys.exit()
 inputdir = sys.argv[1]
 outputdir = sys.argv[2]
+st = int(sys.argv[3])
+e = int(sys.argv[4])
+
 # Check they are directories
 if not(os.path.isdir(inputdir) & os.path.isdir(outputdir)) :
     print("Parameters are not directories!")
     sys.exit()
 
+print(st+e)
 #---------------------------------------------------------------------
 #CODE FOR CALCULATION OF LINKING NUMBER
 #---------------------------------------------------------------------
@@ -148,14 +153,14 @@ def seqlkcalc(point_list):
 # - SOME LIST OF LINKING NUMBERS
 #---------------------------------------------------------------------
 files = list(gemmi.CifWalk(inputdir))
-print(files)
 
 #Creates list of file names
 
 file_list = []
 link_list = []
+print(st+e)
 
-for file_path in files:
+for file_path in files[st:e]:
     file_path_split = file_path.split("/")
     file_name = file_path_split[len(file_path_split)-1]
     file_list.append(file_name)
@@ -185,34 +190,39 @@ for file_path in files:
 
     for c in cent:
         cent_points.append([round(c[0],3), round(c[1], 3),round(c[2],3)])
-
-    link_list.append(seqlkcalc(cent_points))
-
-print(link_list)
-
-link_dic = dict(zip(file_list,link_list))
-
-with open(outputdir + '/lkno.csv', 'w') as f:
-    for key in link_dic.keys():
-        f.write("%s,%s\n"%(key,link_dic[key]))
         
 #---------------------------------------------------------------------
-#GIVE PAIRWISE RMSD AND POWDER COMPARISONS BETWEEN ALL CIF FILES
-#IN INPUT DIRECTORY. ALSO GIVE ABSOLUTE DIFFERENCE OF THE TWO MAXIMUM
-#LINKING NUMBERS CALCULATED FOR EACH FILE BY BY THE SEQLINKCALC
-#FUNCTION 
+#FOR FILES start TO end IN THE INPUT DIRECTORY
+#EXPORT PAIRWISE COMPARISON TABLES FOR RMSD, POWDER AND LK
+#IF ALL SET TO TRUE.
+
+#DEFAULT SETTINGS RUN POWDER AND RMSD FOR ALL FILES IN DIRECTORY
 #---------------------------------------------------------------------
     
+def linkcomp(start, end, pcomp=True, rcomp=True, lktest = False):
 
-full_list = []
-for first_comp in files:
-    i = files.index(first_comp)
-    rest_list = files[i+1:]
-    for second_comp in rest_list:
+    full_list = []
+    file_extract = files[start:end]
+    filename_split = [i.split("/") for i in file_extract]
+    filename_list = [str(i[len(i)-1][4:-4]) for i in filename_split]
+
+    if lktest:
+        lkarray = np.zeros((len(file_extract), len(file_extract)))
+
+    if pcomp:
+        powdarray = np.zeros((len(file_extract), len(file_extract)))
+
+    if rcomp:
+        rmsdarray = np.zeros((len(file_extract), len(file_extract)))
+
+    for first_comp in file_extract:
+        i = file_extract.index(first_comp)
+        rest_list = file_extract[i+1:end]
+        new_entry = []
+        for second_comp in rest_list:
+            j = file_extract.index(second_comp)
             first_comp_split = first_comp.split("/")
             second_comp_split = second_comp.split("/")
-            first_file_name = first_comp_split[len(first_comp_split)-1]
-            second_file_name = second_comp_split[len(second_comp_split)-1]
             one_crys_read = CrystalReader(first_comp)
             two_crys_read = CrystalReader(second_comp)
             one_crys = one_crys_read[0]
@@ -224,26 +234,43 @@ for first_comp in files:
             comp = similarity_engine.compare(one_crys,two_crys)
             powd = powder_sim.similarity(powder_comp)
 
-            try:
-                lkcomp = round(abs(max(link_dic[first_file_name]) - max(link_dic[second_file_name])),3)
-            except TypeError:
-                lkcomp = 'n/a'
-
-            if powd is None:
-                pow_input = ' n/a '
-            else:
-                pow_input = round(powd,3)
-
-            if comp is None:
-                comp_input = 'n/a'
-            else:
-                comp_input = round(comp.rmsd, 3)
-
-            full_list.append([first_file_name, second_file_name, comp_input, pow_input, lkcomp])
+            if lktest:
+                try:
+                    lkarray[j][i] += (round(abs(max(link_dic[first_file_name]) - max(link_dic[second_file_name])),3))
+                except TypeError:
+                    lkarray[j][i] += np.nan
 
             
-            with open(outputdir + '/powdercomp.csv','w',newline = '') as p:
-                powdwriter = csv.writer(p,delimiter=',',
-                            quoting=csv.QUOTE_NONNUMERIC)
-                powdwriter.writerows(full_list)
+            if pcomp:
+                if powd is None:
+                    powdarray[j][i] += np.nan
+                else:
+                    powdarray[j][i]+= (round(powd,3))
 
+            if rcomp:
+                if comp is None:
+                    rmsdarray[j][i]+= np.nan
+                else:
+                    rmsdarray[j][i]+=(round(comp.rmsd, 3))
+
+   
+            
+    if lktest:
+        lkframe = pd.DataFrame(lkarray, index=filename_list, columns = filename_list)
+        lkframe.to_csv(outputdir+"/link_comparison.csv", index = True, header = True, sep = ',')
+
+    if pcomp:
+        powdframe = pd.DataFrame(powdarray, index=filename_list, columns = filename_list)
+        powdframe.to_csv(outputdir+"/powder_comparison.csv", index = True, header = True, sep = ',')
+
+    if rcomp:
+        powdframe = pd.DataFrame(rmsdarray, index=filename_list, columns = filename_list)
+        powdframe.to_csv(outputdir+"/rmsd_comparison.csv", index = True, header = True, sep = ',')
+
+
+
+
+#---------------------------------------------------------------------
+#RUN LKCOMP OUTPUTS WITH DEFAULT OUTPUT SETTINGS ON FIRST n FILES
+#---------------------------------------------------------------------
+linkcomp(st,e)
